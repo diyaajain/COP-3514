a01cd8b3c4f0b206aab0896d0cd2d9ad00801729
diyaajain <diyajain@usf.edu>
2022-02-27 23:22:03 -0500

    made some changes

--- project4_inventory.c
+++ project4_inventory.c
@@ -1,75 +1,85 @@
-#include <stdio.h>
-int find_minimum(int *a, int n);
+/* Name: Diya Jain U81673988*/
+/* This programs tells us how many computers can be made by the entered number of parts and part types. */
 
-int find_minimum(int *a, int n)
-{
-    int *p;  
-    int minimum = a
-    *minimum = *a;
-    for (p = a; p < a+n; p++)
-    {
-        if (*p(a+n) <= *p(a+n+1))
-        {
-            printf("working");
-            minimum = *p;
-        }
-        else
-        {
-            continue;
-        }
-    }
-    return minimum;
-}
+#include <stdio.h>
+
+int find_minimum(int *a, int n); 
 
 int main(){
     int N;
     int K;
     int i,j;
     int freq[10000]={0};
-    int size, count;
-    printf("Enter number of parts (N): ");
+    int count;
+    printf("Enter number of parts (N): "); /* asks the user for the number of parts */
     scanf("%d", &N);
     if (N<1 || N>1000000){
+        printf("Invalid number of parts %d \n", N);
         printf("Must be in the range [1,1000000] \n");
         return 1;
     }
-    printf("Enter number of part types (K): ");
+    printf("Enter number of part types (K): "); /* asks the user for the part type */
     scanf("%d", &K);
     if (K<1 || K>10000){
         printf("Invalid number of part types %d \n", K);
         printf("Must be in the range [1,10000]\n");
         return 1;
     }
+    
     int part_list[N];
-    printf("Enter part list: \n");
+    printf("Enter part list: \n"); /* asks the user to enter the part list */
     for (i = 0; i<N; i++)
-    {
-    scanf("%d", &part_list[i]);
-    for(i=0; i<N; i++)
-    {
-        count = 1;
-        for(j=i+1; j<N; j++)
         {
-            if(part_list[i] == part_list[j]){
-                count++;
-                freq[j] = 0;
+            scanf("%d", &part_list[i]);
+            if(part_list[i]<=0 || part_list[i]>K)
+            {
+                printf("Invalid list of parts \n");
+                printf("Part codes must be in the range [1,%d]\n", K);
+                return 1;
             }
         }
-        if(freq[i] != 0)
-        {
-            freq[i] = count;
-        }
-        printf("\nFrequency of all elements of array : \n");
-    for(i=0; i<N; i++)
-    {
-        if(freq[i] != 0)
+        
+        
+    for (i=0; i<N; i++) /* this for loops finds the total number of occurences of the element in the array i.e frequency */
         {   
-            printf("woring");
-            printf("%d occurs %d times\n", part_list[i], freq[i]);
+            count=0;
+            
+            for (j=0; j<N;j++)
+            {
+                if (part_list[i] == part_list[j])
+                    {
+                        count+=1;
+                    }
+                else 
+                {
+                    continue;
+                }
+            }
+            freq[i]= count;
+        }
+    printf("The factory can build %d computer(s)",find_minimum (freq,N)); /* prints the total number of computers that can be built */
+        
+    return 0; 
+}
+
+/* Finds the minimum value in the frequency array */
+
+int find_minimum(int *a, int n) \
+{
+    int *p;  
+    int *minimum;
+    minimum = a;
+
+    for (p = a ; p < a+n; p++)
+    {
+        if (*p <= *minimum)
+        {
+            *minimum = *p;
+        }
+        else
+        {
+            continue;
         }
     }
-    }
-    }
-    printf("The factory can build %d computer(s)",find_minimum (freq,K));
-    return 0; 
+    return *minimum; /* Return the value which is minimum in the array */
 }
