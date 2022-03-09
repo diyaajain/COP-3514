1a16debf7bc4e6d54397bf5bfbc8eb862ab4485a
diyaajain <diyajain@usf.edu>
2022-03-09 12:40:16 -0500

    Update project5_in_order.c

--- project5_in_order.c
+++ project5_in_order.c
@@ -1,3 +1,6 @@
+/* Name: Diya Jain U81673988 */ 
+/* This is my program for the second question of Project 2. */
+/* In this program I have checked to see if the word is in alphabetical order or not. */
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -5,53 +8,37 @@
 
 int in_order(char *word);
 
-int in_order(char *word)
+int in_order(char *word) /* made the function */
 {
-    int p;
-    int flag =0;
-    int length = strlen(word);
+    int p = 1;
     *word = tolower(*word);
-    while(*word != '\0')
+    while(word[p] != '\0') 
     {
-        if (*word >= 'a' && *word <= 'z')
+        if((word[p]) > (word[p-1]))
         {
-            for (p=0; p<length; p++)
-            {
-                if((word[p]) < (word[p+1]))
-                {
-                    flag = 0;
-                    break;;                    
-                }
-                else
-                {
-                    flag = 1;
-                }
-            }
+            return 1;                
         }
+        p++;
     }
-    return flag;
+
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {   
-    int flag = 0; 
-    printf("%d \n", argc); // remove this in terminal
-    if (argc >2 || argc == 1)
+    if (argc >2 || argc == 1) /* displays if wrong number of sruguments are entered*/
     {
         printf("Incorrect number of arguments. Usage: ./a.out word \n");
         return 1;
     }
-    else{
-        char *word = argv[1];
-        flag = in_order(word);
-        if (flag == 0)
-        {
-            printf("In order");
-        }
-        else
-        {
-            printf("Not in order");
-        }
+    char *word = argv[1];
+    if (in_order(word) == 1)
+    {
+        printf("In order\n");
+    }
+    else 
+    {
+        printf("Not in order\n");
     }
     return 0;
 }
